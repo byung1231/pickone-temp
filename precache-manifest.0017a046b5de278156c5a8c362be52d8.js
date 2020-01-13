184646f7f95bec652d12b16073d3f3d3ced9e788
Byung Yoo <byung1231@gmail.com>
2020-01-13 02:16:22 -0500

    Updates

---
+++ precache-manifest.0017a046b5de278156c5a8c362be52d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10035dc7ce95bda5e79c8fe8142956b7",
+    "url": "/pickone-temp/index.html"
+  },
+  {
+    "revision": "09940ebb8216e78a90f6",
+    "url": "/pickone-temp/static/css/main.4f875277.chunk.css"
+  },
+  {
+    "revision": "ed611f434af5ca1317c7",
+    "url": "/pickone-temp/static/js/2.18296b3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pickone-temp/static/js/2.18296b3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09940ebb8216e78a90f6",
+    "url": "/pickone-temp/static/js/main.424bebfb.chunk.js"
+  },
+  {
+    "revision": "484eb60cfe0a645641dd",
+    "url": "/pickone-temp/static/js/runtime-main.a1f733c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pickone-temp/static/media/logo.5d5d9eef.svg"
+  }
+]);
